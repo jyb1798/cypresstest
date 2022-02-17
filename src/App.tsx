a2796ee7d67bfb099e7a57829aabc13ec4d12598
DESKTOP-HBALMHA\user <vkfks8888@naver.com>
2022-02-17 22:49:52 +0900

    Add: Style/style GlobalStyle 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Main from 'Components/Main/index.Main';
-
+import {GlobalStyle} from 'Style/style';
 
 function App() {
   return (
     <div >
+      <GlobalStyle/>
       energybalance
       <Main/>
     </div>
