a994a1229e0768d71829fd3b2f09312585c3d347
DESKTOP-HBALMHA\user <vkfks8888@naver.com>
2022-02-17 21:24:20 +0900

    🚀 initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Components/Main/index.Main';
+
+
+function App() {
+  return (
+    <div >
+      energybalance
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
