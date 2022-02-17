2b828deacb2435a5bbd62d16245cb073da7ab3da
jyb8509 <bestdung8509@gmail.com>
2022-02-18 00:39:05 +0900

    Add : Nav바 media 쿼리 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Main from "Components/Main/index.Main";
-import Nav from "Components/Nav/index.Nav"
+import Main from "Components/Templates/Main/index.Main";
+import Nav from "Components/Templates/Nav/index.Nav"
 import styled from "styled-components";
 import { GlobalStyle } from "Style/style";
 
