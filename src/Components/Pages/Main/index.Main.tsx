b1d1b74974ec0070aa0fcc80c6191607432fd213
WongueShin <won7963kr@gmail.com>
2022-02-18 11:29:12 +0900

    Modify: /Main

--- src/Components/Pages/Main/index.Main.tsx
+++ src/Components/Pages/Main/index.Main.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import * as S from 'Components/Pages/Main/style.Main';
+import React, { useEffect, useState } from "react";
+import Nav from "Components/Templates/Nav/index.Nav";
+import SearchBar from "Components/Templates/SearchBar/index.SearchBar";
+import styled from "styled-components";
+import { GlobalStyle } from "Style/style";
+import { JsonDataType } from "src/Types";
 
+const Main = () => {
+  const [JsonData, setJsonData] = useState<JsonDataType[]>([]);
+  useEffect(() => {
+    (async () => {
+      await fetch("http://localhost:4000/results")
+        .then((res) => res.json())
+        .then((res) => setJsonData(res));
+    })();
+  }, []);
+  return (
+    <Container>
+      <GlobalStyle />
+      <Nav />
+      <SearchBar JsonData={JsonData} />
+    </Container>
+  );
+};
 
-const Nav:React.FC = () => {
-    return(
-        <>
-        <S.MainDiv>Main</S.MainDiv>
-        </>
-    )
-}
+const Container = styled.div`
 
-export default Nav
+  width: 60vw;
+  margin:auto;
+  
+  @media all and (max-width: 768px) {
+    width: 100vw;
+    margin: auto;
+  }
+`;
+
+export default Main;
