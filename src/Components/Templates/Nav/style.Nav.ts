906656da60eaa097ce2489ad5637bdcf0f6a2f38
jyb8509 <bestdung8509@gmail.com>
2022-02-18 12:04:02 +0900

    Modify: Nav 폴더 모바일 분기점 수정

--- src/Components/Templates/Nav/style.Nav.ts
+++ src/Components/Templates/Nav/style.Nav.ts
@@ -10,7 +10,7 @@ export const LogoContainer = styled.div`
 export const IconContainer = styled.div`
     display: none;
 
-    @media all and (max-width: 672px) {
+    @media all and (max-width: 768px) {
         display: block;
         margin-left : 10px;
         margin-right: 10px;
@@ -23,7 +23,7 @@ export const IconList = styled.div`
     justify-content: center;
     align-items: center;
     
-    @media all and (max-width: 672px) {
+    @media all and (max-width: 768px) {
         display: flex;
         justify-content: space-between;
         align-items: center;
